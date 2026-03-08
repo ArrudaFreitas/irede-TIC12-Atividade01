5f01774efca10ae6c3abe23c5113f47f1cc4f632
ArrudaFreitas <ianarruda7@gmail.com>
2026-03-08 19:16:44 -0300

    refactor(models): refactor constructor user models

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,15 +4,6 @@ export enum Role {
 }
 
 export class User {
-    id: number;
-    username: string;
-    email: string;
-    role: Role;
-
-    constructor(id: number, username: string, email: string, role: Role) {
-            this.id = id;
-            this.username = username;
-            this.email = email;
-            this.role = role;
+    constructor(public id: number, public username: string, public email: string, public role: Role) {
     }
 }
